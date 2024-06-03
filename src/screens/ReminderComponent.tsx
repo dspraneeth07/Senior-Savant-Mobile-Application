9adc4900af68d23633ee15e59c9d5cd64a60872f
Pavan Mahesh <pavanmahesh@Pavans-MacBook-Air.local>
2024-06-04 00:20:33 +0530

    added reminder

--- src/screens/ReminderComponent.tsx
+++ src/screens/ReminderComponent.tsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, Switch, ScrollView } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import PushNotification from 'react-native-push-notification';
 import { RootStackScreenProps } from '../../navigators/RootNavigator';
 
-const ReminderComponentScreen = ( { navigation }: RootStackScreenProps<"ReminderComponentScreen">) => {
+const ReminderComponentScreen = ({ navigation }: RootStackScreenProps<"ReminderComponentScreen">) => {
   const [reminderType, setReminderType] = useState('');
   const [time, setTime] = useState(new Date());
   const [isEveryday, setIsEveryday] = useState(false);
   const [isWeekly, setIsWeekly] = useState(false);
-  const [duration, setDuration] = useState(null); 
+  const [duration, setDuration] = useState(null);
   const [reminders, setReminders] = useState([]);
 
   const handleAddReminder = () => {
@@ -21,35 +20,6 @@ const ReminderComponentScreen = ( { navigation }: RootStackScreenProps<"Reminder
       duration,
     };
     setReminders([...reminders, newReminder]);
-
-    scheduleNotification(newReminder);
-  };
-
-  const scheduleNotification = (reminder) => {
-    const { time, isEveryday, isWeekly, duration } = reminder;
-    let repeatType = null;
-
-    if (isEveryday) {
-      repeatType = 'day';
-    } else if (isWeekly) {
-      repeatType = 'week';
-    }
-
-    const notificationTime = new Date();
-  notificationTime.setHours(time.getHours());
-  notificationTime.setMinutes(time.getMinutes());
-  notificationTime.setSeconds(0);
-
-  if (notificationTime < new Date()) {
-    notificationTime.setDate(notificationTime.getDate() + 1);
-  }
-
-  PushNotification.localNotificationSchedule({
-    message: reminderType,
-    date: notificationTime,
-    repeatType: repeatType,
-    repeatTime: duration,
-    });
   };
 
   return (
